5c8c96ecf774ee464ae1f268bf9fd4f5911a201c
Dhakshineswari <139868245+Dhakshineswari@users.noreply.github.com>
2024-07-30 21:48:30 +0530

    Create sign_in_screen.dart

---
+++ sign_in_screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'news_list_screen.dart';
+
+class SignInScreen extends StatefulWidget {
+  @override
+  _SignInScreenState createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  void _signIn() {
+    if (_usernameController.text == 'user' &&
+        _passwordController.text == 'password') {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => NewsListScreen()),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Invalid username or password')),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sign In'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(labelText: 'Username'),
+            ),
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _signIn,
+              child: Text('Sign In'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.teal,
+                textStyle: TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
